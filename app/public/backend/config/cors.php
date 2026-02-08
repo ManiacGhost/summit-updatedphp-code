5594d8a04e8e51907709390083b99089ab14ebd8
ManiacGhost <harshpandey6754@gmail.com>
2026-02-08 20:34:48 +0530

    allowed cors

--- app/public/backend/config/cors.php
+++ app/public/backend/config/cors.php
@@ -4,8 +4,8 @@
 return [
     'paths' => ['api/*', 'admin/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:3000', 'http://127.0.0.1:3000'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => [],
+    'allowed_origins_patterns' => ['*'],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 
